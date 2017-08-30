fbca4a15e3eef50fd2739fb67d5883a5649359ef
shenzekun <726664809@qq.com>
2017-08-30 16:06:32 +0800

    add see-weather

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # see-weather
 一个命令行查天气预报的小 demo
+
+
+安装：
+
+```
+npm install -g see-weather
+```
+
+>可以查本地天气,在终端输入
+
+```
+weather
+```
+
+>可以查其他位置的天气，例如在终端输入
+
+```
+weather 北京
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+var axios = require('axios');
+
+var data = {};
+if (process.argv[2]) {
+    data.params = {
+        city: process.argv[2]
+    }
+}
+
+
+axios.get('http://api.jirengu.com/weather.php', data).then(function (res) {
+    var weather=res.data.results[0].weather_data[0];
+    console.log("当前所处位置："+res.data.results[0].currentCity +" "+weather.temperature);
+    console.log("今天"+weather.date);
+    console.log(weather.wind);
+    console.log(weather.weather);
+    console.log("建议:"+res.data.results[0].index[0].des);
+}).catch(function (err) {
+    console.log(err);
+})
