30fb7e8757461bccaab5d534766d4f32241c81e6
shenzekun <726664809@qq.com>
2017-08-30 18:38:00 +0800

    modify indexjs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var axios = require('axios');
 
 var data = {};
@@ -10,12 +11,12 @@ if (process.argv[2]) {
 
 
 axios.get('http://api.jirengu.com/weather.php', data).then(function (res) {
-    var weather=res.data.results[0].weather_data[0];
-    console.log("当前所处位置："+res.data.results[0].currentCity +" "+weather.temperature);
-    console.log("今天"+weather.date);
+    var weather = res.data.results[0].weather_data[0];
+    console.log("位置：" + res.data.results[0].currentCity + " " + weather.temperature);
+    console.log("今天" + weather.date);
     console.log(weather.wind);
     console.log(weather.weather);
-    console.log("建议:"+res.data.results[0].index[0].des);
+    console.log("建议:" + res.data.results[0].index[0].des);
 }).catch(function (err) {
     console.log(err);
 })
