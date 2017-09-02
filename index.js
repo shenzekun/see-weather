4135a1bb0c6ed787fbd6c946b3a72bb80b96d4da
shenzekun <726664809@qq.com>
2017-09-02 22:55:51 +0800

    modify index.js

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ if (process.argv[2]) {
 
 
 axios.get('http://api.jirengu.com/weather.php', data).then(function (res) {
-    var weather = res.data.results[0].weather_data[0];
-    console.log("位置：" + res.data.results[0].currentCity + " " + weather.temperature);
-    console.log("今天" + weather.date);
-    console.log(weather.wind);
-    console.log(weather.weather);
-    console.log("建议:" + res.data.results[0].index[0].des);
+    if (res.data.status == 200) {
+        var weather = res.data.results[0].weather_data[0];
+        console.log("位置：" + res.data.results[0].currentCity + " " + weather.temperature);
+        console.log("今天" + weather.date);
+        console.log(weather.wind);
+        console.log(weather.weather);
+        console.log("建议:" + res.data.results[0].index[0].des);
+    } else {
+        console.log(res.data.message);
+    }
 }).catch(function (err) {
     console.log(err);
 })
